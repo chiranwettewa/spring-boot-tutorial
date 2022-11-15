09171a98ee8f683a6a463be9e8bd394e57952137
Chiran_105626 <Chiran_105626@105626-002L>
2022-11-15 21:23:10 +0530

    handle exception

--- src/main/java/com/chiran/Springboottutorial/controller/HelloController.java
+++ src/main/java/com/chiran/Springboottutorial/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.chiran.Springboottutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${welcome.message}")
+    private String message ;
     @GetMapping("/")
     public String helloworld() {
-        return "hello world ss";
+        return message;
     }
 }
